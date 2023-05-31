49673629fbf2dd50db611e4b8b06828baa4aa912
rikimaru45kg <rikimaru2446@hotmail.com>
2023-05-31 11:51:56 +0900

    [new] float calculaton error

--- test.cpp
+++ test.cpp
@@ -3,28 +3,13 @@ using namespace std;
 #define rep(i, N) for (int i=0; i<(int)(N); i++)
 #define rep2(i, A, B) for (int i=(int)(A); i < (int)(B); i++)
 
-#define PI 3.141592653589793
-
 int main() {
-    double T, L, X, Y; cin >> T >> L >> X >> Y;
-    int Q; cin >> Q;
+    long double a, b, c; cin >> a >> b >> c;
 
-    vector<double> E(Q);
-    rep (i, Q) {
-        cin >> E.at(i);
+    if (log2(a) < b*log2(c)) {
+        cout << "Yes" << endl;
     }
-
-    rep (i, Q) {
-        double y, z, theta;
-        y = -L/2 * sin(2*PI*E.at(i)/T);
-        z = L/2 * (1 - cos(2*PI*E.at(i)/T));
-
-        double a;
-        a = sqrt(X*X + (Y-y)*(Y-y));
-        theta = abs(atan(z/a) * 180.0 / PI);
-
-        cout << fixed << setprecision(15) << theta << endl;
-
+    else {
+        cout << "No" << endl;
     }
-
 }
--- try.cpp
+++ try.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main() {
 
-    cout << abs(-5) << endl;
+    cout << log2(4) << endl;
 
 }
