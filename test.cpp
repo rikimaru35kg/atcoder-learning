aba87de36a577de89f1c2a678bae026276f15d1a
rikimaru45kg <rikimaru2446@hotmail.com>
2023-05-31 14:59:02 +0900

    [new] 2-part graph

--- test.cpp
+++ test.cpp
@@ -3,13 +3,56 @@ using namespace std;
 #define rep(i, N) for (int i=0; i<(int)(N); i++)
 #define rep2(i, A, B) for (int i=(int)(A); i < (int)(B); i++)
 
-int main() {
-    long double a, b, c; cin >> a >> b >> c;
+vector<int> colors;
 
-    if (log2(a) < b*log2(c)) {
-        cout << "Yes" << endl;
+void dfs(vector<vector<int>> &edge, int pos, int color) {
+    colors.at(pos) = color;
+
+    for (auto x: edge.at(pos)) {
+        if (colors.at(x) != -1) continue;
+        dfs(edge, x, 1-color);
+    }
+}
+
+int main() {
+    int N; cin >> N;
+
+    colors.assign(N, -1);
+
+    vector<vector<int>> edge(N);
+    rep (i, N-1) {
+        int a, b; cin >> a >> b; a--; b--;
+        edge.at(a).push_back(b);
+        edge.at(b).push_back(a);
+    }
+
+    dfs(edge, 0, 0);
+
+    int num_color1 = 0;
+    rep (i, N) if (colors.at(i) == 1) num_color1++;
+
+    vector<int> answers;
+    if (num_color1 < N/2) {
+        rep(i, N) {
+            if (colors.at(i) == 0) {
+                answers.push_back(i+1);
+            }
+        }
     }
     else {
-        cout << "No" << endl;
+        rep(i, N) {
+            if (colors.at(i) == 1) {
+                answers.push_back(i+1);
+            }
+        }
     }
+
+    rep (i, answers.size()) {
+        if (i < N/2 - 1) cout << answers.at(i) << " ";
+        else {
+            cout << answers.at(i) << endl;
+            break;
+        }
+    }
+
 }
--- try.cpp
+++ try.cpp
@@ -1,7 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+    vector<int> x, y;
+    int z = 5;
+
 int main() {
+
+
+    x.assign(5, -1);
+    y.resize(5, -2);
 
     cout << log2(4) << endl;
 
