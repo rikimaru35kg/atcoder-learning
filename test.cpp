368741723b0bb0c572ae31831dac24e4ed9c654d
rikimaru45kg <rikimaru2446@hotmail.com>
2023-06-01 13:34:13 +0900

    [new] imos method

--- test.cpp
+++ test.cpp
@@ -6,16 +6,51 @@ using namespace std;
 int main() {
     int N; cin >> N;
 
-    vector<string> S(N);
-    map<string, int> registered;
+    vector<vector<int>> papers(N, vector<int>(4));
     rep (i, N) {
-        cin >> S.at(i);
+        int lx, ly, rx, ry;
+        cin >> lx >> ly >> rx >> ry;
 
-        if (registered.count(S.at(i)) == 0) {
-            cout << i+1 << endl;
-            registered[S.at(i)] = 1;
-        }
-
+        papers.at(i).at(0) = lx;
+        papers.at(i).at(1) = ly;
+        papers.at(i).at(2) = rx;
+        papers.at(i).at(3) = ry;
     }
+
+    vector<vector<int>> area(1010, vector<int>(1010, 0));
+
+    for (auto x: papers) {
+        area.at(x.at(1)).at(x.at(0))++;
+        area.at(x.at(3)).at(x.at(0))--;
+        area.at(x.at(1)).at(x.at(2))--;
+        area.at(x.at(3)).at(x.at(2))++;
+    }
+
+    // x direction
+    rep (i, 1005) {
+        rep (j, 1005) {
+            area.at(i).at(j+1) += area.at(i).at(j);
+        }
+    }
+    // y direction
+    rep (i, 1005) {
+        rep (j, 1005) {
+            area.at(i+1).at(j) += area.at(i).at(j);
+        }
+    }
+
+    map<int, int> ans;
+    rep (i, 1000) {
+        rep (j, 1000) {
+            if (ans.count(area.at(i).at(j)) == 0) ans[area.at(i).at(j)] = 1;
+            else ans[area.at(i).at(j)] ++;
+        }
+    }
+
+    rep(i, N){
+        if (ans.count(i+1) == 0) cout << "0" << endl;
+        else cout << ans[i+1] << endl;
+    }
+ 
 
 }
