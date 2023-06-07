4ccc7c15648b339a31426a2e17bfb91ead4510f9
rikimaru45kg <rikimaru2446@hotmail.com>
2023-06-07 16:30:48 +0900

    [new] mod inverse

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+typedef unsigned long long ull;
 #define vi vector<int>
 #define vs vector<string>
 #define vc vector<char>
@@ -16,7 +17,7 @@ typedef long long ll;
 #define repr(i, n) for (int i = (int)(n) - 1; i >= 0; i--)
 #define repk(i, k, n) for (int i = k; i < (int)(n); i++)
 
-ll modpower(ll a, ll b, ll mod) {
+ll modpow(ll a, ll b, ll mod) {
     // Calculate mod(a^b, mod)
     // a >= 0, b >= 0, mod > 0;
 	ll ans = 1;
@@ -30,65 +31,47 @@ ll modpower(ll a, ll b, ll mod) {
 	return ans;
 }
 
-struct State {
-    ll r, c, dist;
-    vvb visited;
-
-    State(ll H, ll W) {
-        visited.resize(H, vb(W, false));
+ll digits(ll x) {
+    ll ret = 0;
+    if (x == 0) return 1;
+    while (x > 0) {
+        x /= 10;
+        ret++;
     }
-
-};
-
-bool movable(ll r, ll c, vvc &tmap) {
-    if (r >= tmap.size() || r < 0) return false;
-    if (c >= tmap.at(0).size() || c < 0) return false;
-    if (tmap.at(r).at(c) == '#') return false;
-
-    return true;
+    return ret;
 }
 
-vl dr = {-1, 0, 1, 0};
-vl dc = {0, 1, 0, -1};
-
-vvl used;
-
-ll dfs(ll r0, ll c0, ll r1, ll c1, vvc &tmap) {
-    if (r0 == r1 && c0 == c1) {
-        if (used.at(r0).at(c0)) return 0;
-    }
-
-    used.at(r1).at(c1) = true;
-    ll dist_max = -1;
-    rep (i, 4) {
-        ll r2 = r1 + dr.at(i);
-        ll c2 = c1 + dc.at(i);
-        if (movable(r2, c2, tmap) || (r0 == r2 && c0 == c2)) {
-            if (used.at(r2).at(c2) && (r0 != r2 || c0 != c2)) continue;
-            ll v = dfs(r0, c0, r2, c2, tmap);
-            if (v >= 0) dist_max = max(dist_max, v + 1);
-        }
-    }
-
-    used.at(r1).at(c1) = false;
-    return dist_max;
-}
+const ll MOD = 1000000007;
 
 int main() {
-    ll H, W; cin >> H >> W;
-    vvc c(H, vc(W));
-    rep (i, H) rep (j, W) cin >> c.at(i).at(j);
+    ll L, R; cin >> L >> R;
 
-    used.resize(H, vl(W, false));
+    ll d_start = digits(L);
+    ll d_end = digits(R);
 
-    ll dist_max = -1;
-    rep (i, H) {
-        rep (j, W) {
-            if (c.at(i).at(j) == '#') continue;
-            dist_max = max(dist_max, dfs(i, j, i, j, c));
+    ll cnt = 0;
+    repk (i, d_start, d_end + 1) {
+        ll a0, an;
+        if (i == d_start) {
+            a0 = L;
         }
+        else {
+            a0 = 1;
+            rep (j, i - 1) a0 *= 10;
+        }
+        if (i == d_end) {
+            an = R;
+        }
+        else {
+            an = 1;
+            rep (j, i) an *= 10;
+            an--;
+        }
+        ll x = i * (a0%MOD + an%MOD) % MOD * ((an - a0)%MOD + 1) % MOD;
+        x = x * modpow(2, MOD-2, MOD) % MOD;
+        cnt = (cnt + x) % MOD;
     }
 
-    if (dist_max > 0) cout << dist_max << endl;
-    else cout << "-1" << endl;
+    cout << cnt << endl;
+
 }
