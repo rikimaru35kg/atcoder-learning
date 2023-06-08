a2982d32a7a078822acfec8cd1240af06152f0eb
rikimaru45kg <rikimaru2446@hotmail.com>
2023-06-08 14:44:08 +0900

    [new] string operation

--- test.cpp
+++ test.cpp
@@ -13,38 +13,34 @@ typedef unsigned long long ull;
 #define vvb vector<vector<bool>>
 #define vvvi vector<vector<vector<int>>>
 #define vvvl vector<vector<vector<ll>>>
-#define rep(i, N) for (int i=0; i<(int)(N); i++)
-#define repr(i, n) for (int i = (int)(n) - 1; i >= 0; i--)
-#define repk(i, k, n) for (int i = k; i < (int)(n); i++)
+#define rep(i, N) for (ll i=0; i<(ll)(N); i++)
+#define repr(i, n) for (ll i = (ll)(n) - 1; i >= 0; i--)
+#define repk(i, k, n) for (ll i = k; i < (ll)(n); i++)
+
 
 
 int main () {
-    ll N; cin >> N;
     string S; cin >> S;
 
-    if (N == 1) {
-        cout << "0" << endl;
-        return 0;
-    }
-
-    ll p = 1, p0 = 0;
-    ll cnt = 0;
-    char c_init = S.at(p0);
-    rep (i, N) {
-        while (S.at(p) == c_init && p < N - 1) {
-            p++;
+    ll length = 0;
+    rep (i, S.size()) {
+        repk (j, i, S.size()) {
+            if (j == 6) {
+                cout << "";
+            }
+            string S_sub = S.substr(i, j-i+1);
+            // repk (k, i, j+1) S_sub += S.at(k);
+            bool acgt = false;
+            // for (char x: S_sub) {
+            //     if (x != 'A' && x != 'C' && x != 'G' && x != 'T') acgt = false;
+            // }
+            if (regex_match(S_sub, regex("[ACGT]+"))) acgt = true;
+            if (acgt) {
+                length = max(length, j-i+1);
+            }
         }
-        if (p == N-1 && S.at(p) == c_init) break;
-
-        cnt += (N-p)*(p-i);
-
-        c_init = S.at(p);
-        p0 = p;
-        p = p + 1;
-        i = p0 - 1;
-
-        if (p >= N) break;
     }
 
-    cout << cnt << endl;
+    cout << length << endl;
+
 }
