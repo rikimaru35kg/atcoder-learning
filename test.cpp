58c2bb03339fc0bd5ccacddc4069d901247cbd96
rikimaru45kg <rikimaru2446@hotmail.com>
2023-06-16 10:10:26 +0900

    [new] N Slimes

--- test.cpp
+++ test.cpp
@@ -19,54 +19,36 @@ typedef unsigned long long ull;
 #define rep1(i, N) for (ll i=1; i<(ll)(N+1); i++)
 #define all(v) (v).begin(), (v).end()
 
-ll N;
-
-ll ringl(ll x) {
-    if (x < 0) return x % N + N;
-    if (x >= 0) return x % N;
-}
-ll ringr(ll x) {
-    if (x <= 0) return x % N + N;
-    if (x <= N) return x;
-    return x % N;
-}
-
 int main() {
-    cin >> N;
-    vl A(2*N);
+    ll N; cin >> N;
+    vl a(N);
+    rep (i, N) cin >> a.at(i);
+
+    vvl dp(N, vl(N+1, 1e18));
+    vvl sum(N, vl(N, 0));
+
     rep (i, N) {
-        cin >> A.at(i); A.at(i+N) = A.at(i);
-    }
-
-    vvl dp(2*N, vl(2*N+1, -1e18));
-
-    rep (i, 2*N) {
         dp.at(i).at(i) = 0;
-        // if (N % 2 == 0) dp.at(i).at(i+1) = -1e18;
-        // else dp.at(i).at(i+1) = A.at(i);
-        dp.at(i).at(i+1) = A.at(i);
+        dp.at(i).at(i+1) = 0;//a.at(i);
     }
-
-    repk (width, 2, 2*N+1) {
-        rep (l, 2*N-width) {
-            if (l == 0 && width == 3) {
-                cout << "";
+    rep (i, N) {
+        repk (j, i, N) {
+            rep (k, j-i+1){
+                sum.at(i).at(j) += a.at(i+k);
             }
-            ll r = l + width;
-            if (width == 2) {
-                dp.at(l).at(r) = max(A.at(l), A.at(r-1));
-                continue;
-            }
-            if (A.at(l) < A.at(r-2)) dp.at(l).at(r) = max(dp.at(l).at(r), dp.at(l).at(r-2) + A.at(r-1));
-            else dp.at(l).at(r) = max(dp.at(r).at(l), dp.at(l+1).at(r-1) + A.at(r-1));
-            if (A.at(l+1) > A.at(r-1)) dp.at(l).at(r) = max(dp.at(l).at(r), dp.at(l+2).at(r) + A.at(l));
-            else dp.at(l).at(r) = max(dp.at(l).at(r), dp.at(l+1).at(r-1) + A.at(l));
         }
     }
 
-    ll max_p = 0;
-    rep (i, N) max_p = max(max_p, dp.at(i).at(i+N));
+    repk(w, 2, N+1) {
+        rep (l, N-w+1) {
+            ll r = l + w;
+            repk (k, l, r) {
+                dp.at(l).at(r) = min(dp.at(l).at(r), dp.at(l).at(k) + dp.at(k).at(r) + sum.at(l).at(r-1));
+            }
+        }
+    }
 
-    cout << max_p << endl;
+    cout << dp.at(0).at(N) << endl;
+
 
 }
