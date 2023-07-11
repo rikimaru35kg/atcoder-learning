3df30cd472c149b8d37ae247c3caf210a7221543
rikimaru45kg <rikimaru2446@hotmail.com>
2023-07-11 10:14:23 +0900

    [update] Shakutori

--- test.cpp
+++ test.cpp
@@ -33,37 +33,25 @@ template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, tr
 const ll INF = 1e18;
 const double PI = 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117;
 
-long long modpow(long long a, long long b, long long mod) {
-	long long ans = 1;
-    a %= mod;
-	while (b > 0) {
-		if ((b & 1) == 1) {
-			ans = ans * a % mod;
-		}
-		a = a * a % mod;
-		b = (b >> 1);
-	}
-	return ans;
-}
-
-ll powmod(ll x, ll t, ll mod) {
-    if (!t) return 1;
-    x %= mod;
-    ll r = powmod(x, t/2, mod);
-    (r *= r) %= mod;
-    if (t%2) (r *= x) %= mod;
-    return r;
-}
-
 int main () {
-    ll MOD = 998244353;
-    ll N, K, M; cin >> N >> K >> M;
+    string S; cin >> S;
+    ll K; cin >> K;
+    ll N = SIZE(S);
+    vl Sb(N, 0);
+    rep (i, N) Sb[i] = (S[i] == '.');
 
-    if (M % MOD == 0) {
-        cout << 0 << endl;
-        return 0;
+    ll right = 0;
+    ll cnt = 0, ans = 0;
+    rep (left, N) {
+        while (right < N) {
+            if (cnt + Sb[right] > K) break;
+            cnt += Sb[right];
+            ++right;
+        }
+        chmax(ans, right-left);
+        cnt -= Sb[left];
     }
-    ll z = modpow(K, N, MOD-1);
-    ll w = modpow(M, z, MOD);
-    cout << w << endl;
+
+    cout << ans << endl;
+    return 0;
 }
