888f8227fe4bf39bcbeb1f6508c7d8b3c2f9944a
rikimaru45kg <rikimaru2446@hotmail.com>
2023-09-01 11:36:08 +0900

    [new] bi graph (two-colored graph)

--- test.cpp
+++ test.cpp
@@ -37,77 +37,53 @@ const double PI = 3.141592653589793238462643383279502884197169399375105820974944
 // using namespace atcoder;
 // using mint = modint998244353;
 
-struct BIT {
-    ll n;
-    vl bit;
-    BIT (ll _n): n(_n+1), bit(_n+1) {}
-
-    void add(ll i, ll x) {
-        for(ll idx = i; idx<n; idx += idx&-idx) {
-            bit[idx] += x;
-        }
-    }
-    ll sum(ll i) {
-        ll ret = 0;
-        for (ll idx=i; idx>0; idx -= idx&-idx) {
-            ret += bit[idx];
-        }
-        return ret;
-    }
-    ll sum_lower_bound(ll x) {
-        ll s = 0, len = 1;
-        while(len<<1 < n) len<<=1;
-        while(len>=1) {
-            if (s+len >= n || bit[s+len] >= x) {
-                len>>=1;
-                continue;
-            }
-            x -= bit[s+len];
-            s += len;
-            len >>= 1;
-        }
-        return s+1;
-    }
-};
-
 int main () {
-    ll N, M, K; cin >> N >> M >> K;
-    vl A(N);
-    rep (i, N) cin >> A[i];
-    map<ll,ll> cc, ccr;
-    rep (i, N) cc[A[i]] = 0;
-    ll idx = 1;
-    for (auto [k, v]: cc) {
-        cc[k] = idx;
-        ccr[idx] = k;
-        ++idx;
-    }
-    ll num = SIZE(cc);
-
-    BIT bit(num);
-    BIT bit_sum(num);
+    ll N, M; cin >> N >> M;
+    vvl from(N);
     rep (i, M) {
-        ll a = cc[A[i]];
-        bit.add(a, 1);
-        bit_sum.add(a, ccr[a]);
+        ll u, v; cin >> u >> v;
+        --u; --v;
+        from[u].push_back(v);
+        from[v].push_back(u);
     }
 
-    rep (i, N-M+1) {
-        ll x = bit.sum_lower_bound(K);
-        ll ans = bit_sum.sum(x);
-        ll r = bit.sum(x) - K;
-        ans -= r * ccr[x];
-        printf("%lld ", ans);
+    vvl graph;
+    ll graph_num = 0;
 
-        if (i == N-M) break;
+    vl seen(N, -1);
+    auto dfs = [&](auto f, ll x, ll z, ll org=-1) -> bool {
+        seen[x] = z;
+        ++graph[graph_num][z];
+        for (auto y: from[x]) {
+            if (y == org) continue;
+            if (seen[y] == -1) {
+                bool b = f(f, y, 1-z, x);
+                if (!b) return false;
+            } else {
+                if (seen[y] == seen[x]) return false;
+            }
+        }
+        return true;
+    };
 
-        ll a0 = cc[A[i]];
-        ll a1 = cc[A[i+M]];
-        bit.add(a0, -1);
-        bit.add(a1, 1);
-        bit_sum.add(a0, -ccr[a0]);
-        bit_sum.add(a1, ccr[a1]);
+    rep (i, N) {
+        if (seen[i] != -1) continue;
+        graph.push_back(vl(2));
+        bool b = dfs(dfs, i, 0);
+        if (!b) {
+            puts("0");
+            return 0;
+        }
+        ++graph_num;
     }
-    cout << endl;
+
+    ll ans = N*(N-1)/2 - M;
+    rep (i, graph_num) {
+        ll n0 = graph[i][0];
+        ans -= n0*(n0-1)/2;
+        ll n1 = graph[i][1];
+        ans -= n1*(n1-1)/2;
+    }
+    cout << ans << endl;
 
 }
