5593f6b4cf34096a26e38bd83e18a44b42087b27
rikimaru35kg <rikimaru35kg@gmail.com>
2024-06-14 14:10:10 +0900

    [review] parity

--- test.cpp
+++ test.cpp
@@ -189,10 +189,46 @@ Pr operator- (Pr a, Pr b) {return {a.first-b.first, a.second-b.second};}
 Pr operator* (Pr a, Pr b) {return {a.first*b.first, a.second*b.second};}
 Pr operator/ (Pr a, Pr b) {return {a.first/b.first, a.second/b.second};}
 
-#include <atcoder/lazysegtree>
-using namespace atcoder;
-#include <atcoder/fenwicktree>
+//! Only when <= 1e6
+//! If not, use Combination2 class below.
+class Combination {
+    long long mx, mod;
+    vector<long long> facts, ifacts;
+public:
+    // argument mod must be a prime number!!
+    Combination(long long mx, long long mod): mx(mx), mod(mod), facts(mx+1), ifacts(mx+1) {
+        facts[0] = 1;
+        for (long long i=1; i<=mx; ++i) facts[i] = facts[i-1] * i % mod;
+        ifacts[mx] = modpow(facts[mx], mod-2);
+        for (long long i=mx-1; i>=0; --i) ifacts[i] = ifacts[i+1] * (i+1) % mod;
+    }
+    long long operator()(long long n, long long r) {
+        if (r < 0 || r > n || n < 0 || n > mx) return 0;
+        return facts[n] * ifacts[r] % mod * ifacts[n-r] % mod;
+    }
+    long long nPr(long long n, long long r) {
+        if (r < 0 || r > n || n < 0 || n > mx) return 0;
+        return facts[n] * ifacts[n-r] % mod;
+    }
+    long long get_fact(long long n) {
+        if (n > mx) return 0;
+        return facts[n];
+    }
+    long long get_factinv(long long n) {
+        if (n > mx) return 0;
+        return ifacts[n];
+    }
+    long long modpow(long long a, long long b) {
+        if (b == 0) return 1;
+        a %= mod;
+        long long child = modpow(a, b/2);
+        if (b % 2 == 0) return child * child % mod;
+        else return a * child % mod * child % mod;
+    }
+};
+
 #include <atcoder/modint>
+using namespace atcoder;
 using mint = modint998244353;
 using vm = vector<mint>;
 using vvm = vector<vector<mint>>;
@@ -205,41 +241,28 @@ inline void debug_view(vm &v){for(auto e: v){cerr << e.val() << " ";} cerr << en
 inline void debug_view(vvm &vv){cerr << "----" << endl;for(auto &v: vv){debug_view(v);} cerr << "--------" << endl;}
 #endif
 
-struct S {
-    mint x, w;
-    S(ll x, ll w): x(x),w(w) {}
-    S(mint x, mint w): x(x),w(w) {}
-};
-S op(S a, S b) {
-    return S(a.x+b.x, a.w+b.w);
-}
-S e() {return S(0,0);}
-using F = ll;
-S mapping(F f, S x) {
-    return S(x.x+f*x.w, x.w);
-}
-F composition(F f, F g) {return f+g;}
-F id() {return 0;}
-
 int main () {
     // ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    LONG(N); VL(A, N);
-    ll M = 2e5+10;
-    vector<S> v(N, S(0,1));
-    lazy_segtree<S,op,e,F,mapping,composition,id> seg(v);
-    fenwick_tree<ll> tree(M), tree2(M);
-    mint sum = 0;
-    rep(i, N) {
-        mint plus = tree.sum(0, A[i]+1) * A[i];
-        plus += tree2.sum(A[i]+1, M);
-        plus *= 2;
-        sum += plus + A[i];
-        Out(sum/(i+1)/(i+1));
-
-        tree.add(A[i], 1);
-        tree2.add(A[i], A[i]);
+    ll Z = 2e5+10;
+    Combination comb(Z, 998244353);
+    LONG(N, M, K);
+    vl deg(N);
+    rep(i, M) {
+        LONGM(a ,b);
+        deg[a]++; deg[b]++;
     }
+    ll even = 0, odd = 0;
+    rep(i, N) {
+        if(deg[i]%2==0) even++;
+        else odd++;
+    }
+    mint ans = 0;
+    for(ll i=0; i<=K; i+=2) {
+        mint now = comb(odd, i) * comb(even, K-i);
+        ans += now;
+    }
+    Out(ans);
 
     
 }
