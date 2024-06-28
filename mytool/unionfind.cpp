97a92e1e67ca3821f9899f0b027d3b0cb73abb93
rikimaru35kg <rikimaru35kg@gmail.com>
2024-06-28 16:35:28 +0900

    [update] weighted union find

--- mytool/unionfind.cpp
+++ mytool/unionfind.cpp
@@ -25,8 +25,8 @@ struct UnionFind {
 };
 
 struct WeightedUnionFind {
-    vector<long long> p, num, diff;
-    WeightedUnionFind(long long n) : p(n,-1), num(n,1), diff(n,0) {}
+    vector<long long> p, num, diff; vector<bool> inf;
+    WeightedUnionFind(long long n) : p(n,-1), num(n,1), diff(n), inf(n) {}
     long long leader (long long x) {
         if (p[x] == -1) return x;
         long long y = p[x];
@@ -38,11 +38,15 @@ struct WeightedUnionFind {
         leader(x); leader(y);  // path compression, -> diff will be based on root.
         w = diff[y] - diff[x] - w;  // p[x]->x->y->p[y]
         x = leader(x); y = leader(y);
-        if (x == y) return w == 0;
+        if (x == y) {
+            if(w != 0) inf[x] = true;  // component x has infinite cycle
+            return w == 0;
+        }
         if (size(x) > size(y)) swap(x, y), w = -w; // new parent = y
         diff[x] = w;
         p[x] = y;
         num[y] += num[x];
+        if(inf[x]) inf[y] = true;
         return true;
         // merge関数はポテンシャルの差として引数を指定すれば良い
         // yに対してxのポテンシャルはw大きい
@@ -55,7 +59,6 @@ struct WeightedUnionFind {
     bool same (long long x, long long y) { return leader(x) == leader(y); }
     long long size (long long x) { return num[leader(x)]; }
 };
-
 
 int main () {
     UnionFind uf(100);
