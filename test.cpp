98af7d09b6c164d039233266d865abe518947b6d
rikimaru35kg <rikimaru35kg@gmail.com>
2024-07-11 10:37:15 +0900

    [review] LCM/GCD

--- test.cpp
+++ test.cpp
@@ -197,116 +197,33 @@ Pr operator- (Pr a, Pr b) {return {a.first-b.first, a.second-b.second};}
 Pr operator* (Pr a, Pr b) {return {a.first*b.first, a.second*b.second};}
 Pr operator/ (Pr a, Pr b) {return {a.first/b.first, a.second/b.second};}
 
-//! BE CAREFUL ABOUT OVERFLOWING!
-//! repeated usage of +-*/ leads to overflowing
-//! Do not repeat +-*/ more than one time (suppose p,q<=|1e9|)
-//! BE CAREFUL ABOUT CALCULATION COST!
-//! O(logM) just for initializing
-struct Frac {
-    long long p, q;  // p/q: p over q (like y/x: y over x)
-    Frac(long long a=0, long long b=1) {
-        if (b == 0) {
-            p = 1; q = 0;  // inf (no definition of -inf)
-            return;
-        }
-        long long g = gcd(a, b);
-        p = a/g; q = b/g;
-        if (q<0) {p=-p; q=-q;}
-    }
-    Frac operator+(const ll x) {
-        if (q == 0) return Frac(1, 0);
-        return *this + Frac(x);
-    }
-    Frac operator+(const Frac &rhs) {
-        if (q == 0 || rhs.q == 0) return Frac(1, 0);
-        return Frac(p*rhs.q + q*rhs.p, q*rhs.q);
-    }
-    Frac operator-(const ll x) {
-        if (q == 0) return Frac(1, 0);
-        return *this - Frac(x);
-    }
-    Frac operator-(const Frac &rhs) {
-        if (q == 0 || rhs.q == 0) return Frac(1, 0);
-        return Frac(p*rhs.q - q*rhs.p, q*rhs.q);
-    }
-    Frac operator*(const ll x) {
-        if (q == 0) return Frac(1, 0);
-        return Frac(p*x, q);
-    }
-    Frac operator*(const Frac &rhs) {
-        if (q == 0 || rhs.q == 0) return Frac(1, 0);
-        return Frac(p*rhs.p, q*rhs.q);
-    }
-    Frac operator/(const ll x) {
-        if (q == 0 || x == 0) return Frac(1, 0);
-        return Frac(p, q*x);
-    }
-    Frac operator/(const Frac &rhs) {
-        if (q == 0 || rhs.p == 0) return Frac(1, 0);
-        return Frac(p*rhs.q, q*rhs.p);
-    }
-    bool operator<(const ll x) const {
-        return *this < Frac(x);
-    }
-    bool operator<(const Frac &rhs) const {
-        return p*rhs.q - q*rhs.p < 0;
-    }
-    bool operator<=(const ll x) const {
-        return *this <= Frac(x);
-    }
-    bool operator<=(const Frac &rhs) const {
-        return p*rhs.q - q*rhs.p <= 0;
-    }
-    bool operator>(const ll x) const {
-        return *this > Frac(x);
-    }
-    bool operator>(const Frac &rhs) const {
-        return p*rhs.q - q*rhs.p > 0;
-    }
-    bool operator>=(const ll x) const {
-        return *this >= Frac(x);
-    }
-    bool operator>=(const Frac &rhs) const {
-        return p*rhs.q - q*rhs.p >= 0;
-    }
-    bool operator==(const ll x) const {
-        return (q==1 && p==x);
-    }
-    bool operator==(const Frac &rhs) {
-        return (p==rhs.p && q==rhs.q);
-    }
-};
+#include <atcoder/segtree>
+using namespace atcoder;
+
+using S = ll;
+S op(S a, S b){return gcd(a,b);}
+S e() {return 0;}
 
 int main () {
     // ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    LONG(N, K);
-    VP(POS, N);
-    if(K==1) {
-        puts("Infinity"); return 0;
+    LONG(N, Q);
+    VL(A, N);
+    VL(B, N);
+    vl da(N-1), db(N-1);
+    rep(i, N-1) da[i] = A[i+1] - A[i];
+    rep(i, N-1) db[i] = B[i+1] - B[i];
+    segtree<S,op,e> sega(da), segb(db);
+    rep(i, Q) {
+        LONGM(h1, h2, w1, w2);
+        ll aw = h2-h1, bw = w2-w1;
+        ll ans = A[h1] + B[w1];
+        de4(h1,h2,w1,w2);
+        de2(aw, bw)
+        ans = gcd(ans, sega.prod(h1, h1+aw));
+        ans = gcd(ans, segb.prod(w1, w1+bw));
+        Out(ans);
     }
-    map<pair<Frac,Frac>,ll> lines;
-    map<ll,ll> xlines;
-    rep(i, N) rep(j, i) {
-        auto [x1, y1] = POS[i];
-        auto [x2, y2] = POS[j];
-        if(x1==x2) {
-            xlines[x1]++;
-        } else {
-            Frac a = Frac(y1-y2, x1-x2);
-            Frac b = Frac(y1) - a*x1;
-            lines[{a,b}]++;
-        }
-    }
-    de2(SIZE(xlines), SIZE(lines))
-    ll ans = 0;
-    for(auto [_,v]: lines) {
-        if(v>=K*(K-1)/2) ++ans;
-    }
-    for(auto [_,v]: xlines) {
-        if(v>=K*(K-1)/2) ++ans;
-    }
-    Out(ans);
     
 }
 
