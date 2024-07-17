4e87af7e8c432eb7be50659ba3f54071e45244cc
rikimaru35kg <rikimaru35kg@gmail.com>
2024-07-17 14:28:40 +0900

    [update] matrix snippet for typename T

--- mytool/matrix.cpp
+++ mytool/matrix.cpp
@@ -5,6 +5,69 @@ using namespace std;
 //! Currently, only operator* is defined.
 template <typename T>
 class Mat {
+    long long n; vector<vector<T>> a;
+public:
+    // Initialize n*n matrix
+    Mat (long long n, const vector<vector<T>> &mat={})
+    : n(n), a(n, vector<T>(n)) {
+        // unit matrix if mat is not specified
+        if (mat.size() == 0) for (int i=0; i<n; ++i) a[i][i] = 1;
+        else {
+            for (int i=0; i<n; ++i) for (int j=0; j<n; ++j) {
+                a[i][j] = mat[i][j];
+            }
+        }
+    }
+    // Define operator*
+    Mat operator* (const Mat &rhs) {  // Mat * Mat
+        Mat ret(n);
+        ret.a.assign(n, vector<T>(n, 0));  // zero matrix
+        for (int i=0; i<n; ++i) for (int j=0; j<n; ++j) {
+            for (int k=0; k<n; ++k) {
+                ret.a[i][j] += a[i][k] * rhs.a[k][j];
+            }
+        }
+        return ret;
+    }
+    vector<T> operator* (const vector<T> &rhs) {  // Mat * vector
+        vector<T> ret(n, 0);
+        for (int j=0; j<n; ++j) for (int k=0; k<n; ++k) {
+            ret[j] += a[j][k] * rhs[k];
+        }
+        return ret;
+    }
+    // power k (A^k)
+    void pow(long long k) {
+        *this = pow_recursive(*this, k);
+    }
+    Mat pow_recursive(Mat b, long long k) {
+        Mat ret(b.n);
+        if (k == 0) return ret;
+        if (k%2 == 1) ret = b;
+        Mat tmp = pow_recursive(b, k/2);
+        return ret * tmp * tmp;
+    }
+    long long ij(long long i, long long j) {
+        return a[i][j];
+    }
+#ifdef __DEBUG
+    void print(string debugname="------") {  // for debug
+        cerr << n << '\n';
+        cerr << debugname << ":\n";
+        for (int i=0; i<n; ++i) for (int j=0; j<n; ++j) {
+            cerr << a[i][j] << (j==n-1? '\n': ' ');
+        }
+        cerr << "---------" << '\n';
+    }
+#else
+    void print(string debugname="------") {}
+#endif
+};
+
+//! n*n matrix
+//! Currently, only operator* is defined.
+template <typename T>
+class Mat_OLD {
     long long n; vector<vector<T>> a; long long mod;
 public:
     // Initialize n*n matrix
