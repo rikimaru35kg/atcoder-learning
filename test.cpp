d024db67666bc09fa0ea05e83eaa88b2a44068e4
rikimaru35kg <rikimaru35kg@gmail.com>
2025-02-05 14:05:44 +0900

    [update] setting.json autosave

--- test.cpp
+++ test.cpp
@@ -227,59 +227,9 @@ Pr operator- (Pr a, Pr b) {return {a.first-b.first, a.second-b.second};}
 Pr operator* (Pr a, Pr b) {return {a.first*b.first, a.second*b.second};}
 Pr operator/ (Pr a, Pr b) {return {a.first/b.first, a.second/b.second};}
 
-struct Diameter {
-    int n, a, b; bool done=false;
-    long long diam;
-    using PRII = pair<int,int>;
-    using PRLI = pair<long long,int>;
-    vector<vector<PRLI>> from;
-    Diameter(int n): n(n), from(n) {}
-    void add_edge(int a, int b, long long c=1) {
-        from[a].emplace_back(b, c);
-        from[b].emplace_back(a, c);
-    }
-    PRLI dfs(int v, long long d=0, int p=-1) {
-        PRLI ret(d, v);
-        for(auto [nv,c]: from[v]) if(nv!=p) {
-            chmax(ret, dfs(nv, d+c, v));
-        }
-        return ret;
-    }
-    PRII get_end_points() {
-        if(done) return {a,b};
-        done = true;
-        a = dfs(0).second;
-        auto [dtmp, btmp] = dfs(a);
-        b = btmp, diam = dtmp;
-        return {a,b};
-    }
-    int get_diameter() {
-        get_end_points();
-        return diam;
-    }
-    // calculate dist(N) from sv using DFS
-    // &dist(N) is input, but it is return value practically
-    void caldist(int sv, vector<long long> &dist) {
-        auto dfs=[&](auto f, int v, long long d=0, int p=-1) -> void {
-            dist[v] = d;
-            for(auto [nv,c]: from[v]) if(nv!=p) {
-                f(f, nv, d+c, v);
-            }
-        };
-        dfs(dfs, sv);
-    }
-};
-
 void solve() {
     LONG(N);
-    Diameter d(N);
-    rep(i, N-1) {
-        LONGM(a,b);
-        d.add_edge(a,b);
-    }
-    auto [a,b] = d.get_end_points();
-    ll ans = d.get_diameter();
-    printf("%d %d\n", a+1, b+1);
+    Out(N);
 
 }
 
