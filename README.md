e179cd371ae6684292bac41ca18af6f99b0a0bf5
rikimaru35kg <rikimaru35kg@gmail.com>
2026-06-18 18:29:46 +0900

    [review] rank C

--- README.md
+++ README.md
@@ -1183,8 +1183,7 @@
 - 基本 [E - Yet Another Sigma Problem](https://atcoder.jp/contests/abc353/tasks/abc353_e)
 !!! where i am now for rank S (last submission 2026/4/28)
 - !要復習 [E - Forbidden Prefix](https://atcoder.jp/contests/abc403/tasks/abc403_e)
-!!! where i am now for rank C (last submission 2025/3/6)
-- !復習価値低 [G - Edit to Match](https://atcoder.jp/contests/abc377/tasks/abc377_g)
+- 基本 [G - Edit to Match](https://atcoder.jp/contests/abc377/tasks/abc377_g)
 - !復習価値低 [G - Minimum Xor Pair Query](https://atcoder.jp/contests/abc308/tasks/abc308_g)
 
 ## Z-algorithm（Zアルゴリズム）
@@ -1204,6 +1203,7 @@
 - 基本 反転時交互列最大化 [1 - 電飾 (Illumination)](https://atcoder.jp/contests/joi2013ho/tasks/joi2013ho1)
 - 基本 同じ向きの人最大化 [D - Face Produces Unhappiness](https://atcoder.jp/contests/abc140/tasks/abc140_d)
 - !復習価値中（構文木で解く場合） 括弧列内文字反転（ちょっと毛色が異なるためNote非掲載） [F - Transpose](https://atcoder.jp/contests/abc350/tasks/abc350_f)
+!!! where i am now for rank C (last submission 2025/2/27)
 - !復習価値低 [A - とてもたのしい家庭菜園 (Growing Vegetables is Fun 4)](https://atcoder.jp/contests/joi2021ho/tasks/joi2021ho_a)
 
 ## Aho-Corasick
--- test.cpp
+++ test.cpp
@@ -228,141 +228,47 @@ Pr operator- (Pr a, Pr b) {return {a.first-b.first, a.second-b.second};}
 Pr operator* (Pr a, Pr b) {return {a.first*b.first, a.second*b.second};}
 Pr operator/ (Pr a, Pr b) {return {a.first/b.first, a.second/b.second};}
 
-const long long MX = 2;
-const long long ps[12] = {1000000007, 1000000009, 1000000021,
-                          1000000033, 1000000087, 1000000093,
-                          1000000097, 1000000103, 1000000123,
-                          1000000181, 1000000207, 1000000223};
-struct mints {
-    long long data[MX];
-    mints(long long x=0) { for(int i=0; i<MX; ++i) data[i] = (x+ps[i])%ps[i]; }
-    mints operator+(mints x) const {
-        for(int i=0; i<MX; ++i) x.data[i] = (data[i]+x.data[i]) % ps[i];
-        return x;
-    }
-    mints &operator+=(mints x) { *this = *this + x; return *this; }
-    mints operator+(long long x) const { return *this + mints(x); }
-    friend mints operator+(long long a, mints b) { return mints(a)+b; }
-    mints operator-(mints x) const {
-        for(int i=0; i<MX; ++i) x.data[i] = (data[i]-x.data[i]+ps[i]) % ps[i];
-        return x;
-    }
-    mints &operator-=(mints x) { *this = *this - x; return *this; }
-    mints operator-(long long x) const { return *this - mints(x); }
-    friend mints operator-(long long a, mints b) { return mints(a)-b; }
-    mints operator*(mints x) const {
-        for(int i=0; i<MX; ++i) x.data[i] = data[i]*x.data[i]%ps[i];
-        return x;
-    }
-    mints &operator*=(mints x) { *this = *this * x; return *this; }
-    mints operator*(long long x) const { return *this * mints(x); }
-    friend mints operator*(long long a, mints b) { return mints(a)*b; }
-    mints pow(long long x) const {
-        if (x==0) return mints(1);
-        mints ret = pow(x/2);
-        ret = ret * ret;
-        if (x%2==1) ret = ret * *this;
-        return ret;
-    }
-    long long pow(long long a, long long b, long long p) const {
-        if(b==0) return 1;
-        a %= p;
-        long long ret = pow(a, b/2, p);
-        ret = ret * ret % p;
-        if (b%2==1) ret = ret * a % p;
-        return ret;
-    }
-    mints inv() const {
-        mints ret;
-        for(int i=0; i<MX; ++i) {
-            long long p = ps[i];
-            long long x = pow(data[i], p-2, p);
-            ret.data[i] = x;
-        }
-        return ret;
-    }
-    bool operator<(mints x) const {
-        for(int i=0; i<MX; ++i) if (data[i] != x.data[i]) {
-            return data[i] < x.data[i];
-        }
-        return false;
-    }
-    bool operator==(mints x) const {
-        for(int i=0; i<MX; ++i) if (data[i] != x.data[i]) return false;
-        return true;
-    }
-    void print() const {
-        for(int i=0; i<MX; ++i) cerr << data[i] << ' ';
-        cerr << '\n';
-    }
-};
-namespace std {
-template<>
-struct hash<mints> {
-    size_t operator()(const mints &x) const {
-        size_t seed = 0;
-        for(int i=0; i<MX; ++i) {
-            hash<long long> phash;
-            seed ^= phash(x.data[i]) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-        }
-        return seed;
-    }
-};
-}
-istream& operator>>(istream &is, mints &x) { long long a; cin>>a; x = a; return is; }
-ostream& operator<<(ostream& os, mints &x) { os<<x.data[0]; return os; }
-using vm = vector<mints>;
-using vvm = vector<vector<mints>>;
-
-template <class mints> struct RollingHash {
-    int n;
-    long long base;
-    vector<mints> rh, bpow;
-    RollingHash(string s="", long long base=1234567): base(base) {
-        if(s!="") set(s, base);
+struct Trie {
+    struct Node {
+        using MP = map<char,int>;
+        MP to;
+        ll dp;
+        Node(MP to=MP()): to(to), dp(INF) {}
     };
-    void set(string &s, long long _base=1234567) {
-        n = s.size(); rh.resize(n+1,0); bpow.resize(n+1, 1);
-        base = _base;
-        for(int i=0; i<n; ++i) {
-            rh[i+1] = rh[i]*base + s[i];
-            bpow[i+1] = bpow[i] * base;
+    int n;  // # of nodes
+    vector<Node> node;
+    Trie(): n(1), node(1) { node[0].dp = 0; }  // only root node
+    ll add(string &s) {
+        int v = 0;
+        vl vs(1);
+        for(auto c: s) {
+            if(!node[v].to.count(c)) {
+                node.push_back(Node());
+                node[v].to[c] = n;
+                ++n;
+            }
+            v = node[v].to[c];
+            vs.push_back(v);
         }
-    }
-    mints get(int l, int r) { // [l,r)
-        assert(l<=r);
-        return rh[r] - rh[l]*bpow[r-l];
+        reverse(all(vs));
+        ll m = vs.size();
+        ll ret = INF;
+        rep(i, m) chmin(ret, i+node[vs[i]].dp);
+
+        rep(i, m) chmin(node[vs[i]].dp, i);
+
+        return ret;
     }
 };
 
 void solve() {
     LONG(N);
-    STRING(S, T);
-    RollingHash<mints> rht(T);
-
-    string rgb = "RGB";
-    ll ans = 0;
-    rep(k, 3) {
-        string s = S;
-        for(auto &c: s) {
-            if(c==rgb[k]) continue;
-            rep(j, 3) {
-                if(j==k) continue;
-                c ^= rgb[j];
-            }
-        }
-        de(s)de(T)
-        RollingHash<mints> rhs(s);
-        rep(i, N) {
-            if(rhs.get(N-1-i,N)==rht.get(0,i+1)) ++ans;
-            de3(s.substr(N-1-i,i+1), T.substr(0,i+1), ans)
-        }
-        rep(i, N-1) {
-            if(rhs.get(0,i+1)==rht.get(N-1-i,N)) ++ans;
-            de3(s.substr(0,i+1), T.substr(N-1-i,i+1), ans)
-        }
+    Trie trie;
+    rep(i, N) {
+        STRING(s);
+        ll ans = trie.add(s);
+        Out(ans);
     }
-    Out(ans);
 
 }
 
